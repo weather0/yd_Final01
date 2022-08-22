d0eb8b4b1ad039121f9a04d10c7064fe67ed5135
weather0 <af1291309@gmail.com>
2022-08-22 17:54:06 +0900

    update

---
+++ k-project/src/main/java/com/kcy/project/vo/UsersVO.java
@@ -0,0 +1,20 @@
+package com.kcy.project.vo;
+
+import lombok.Data;
+
+@Data
+public class UsersVO {
+
+	String userId;
+	String userType;
+	String userName;
+	String userSsn;
+	String userSex;
+	String userDept;
+	String userAddr;
+	String userPhone;
+	String userEmail;
+	String userPw;
+	String userNation;
+
+}
