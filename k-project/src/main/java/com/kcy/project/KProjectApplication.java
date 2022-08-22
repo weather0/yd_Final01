0877162d1bd2245285a7e44597157928e9a22462
hwang <burnrare@naver.com>
2022-08-22 12:00:21 +0900

    test

---
+++ k-project/src/main/java/com/kcy/project/KProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kcy.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KProjectApplication.class, args);
+	}
+
+}
---
+++ k-project/src/test/java/com/kcy/project/KProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcy.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
