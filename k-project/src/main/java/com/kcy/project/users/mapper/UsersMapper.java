4c8fe41f2af1637de759720a5d2bb6c31e71e012
weather0 <af1291309@gmail.com>
2022-08-23 10:42:50 +0900

    update

--- k-project/src/main/java/com/kcy/project/users/mapper/UsersMapper.java
+++ k-project/src/main/java/com/kcy/project/users/mapper/UsersMapper.java
@@ -1,11 +1,11 @@
-package com.kcy.project.service;
+package com.kcy.project.users.mapper;
 
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.kcy.project.vo.UsersVO;
-@Mapper
+
 public interface UsersMapper {
 	public List<UsersVO> usersList();
 
