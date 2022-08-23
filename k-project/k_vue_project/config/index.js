9ae87955e42dec2d76fc4d7df72db3e6f97ba82c
hwang <burnrare@naver.com>
2022-08-23 15:13:08 +0900

    쓸모없음

--- k-project/k_vue_project/config/index.js
+++ k-project/k_vue_project/config/index.js
@@ -41,7 +41,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../src/main/resources/static'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
--- k-project/k_vue_project/vue.config.js
+++ k-project/k_vue_project/vue.config.js
@@ -2,12 +2,17 @@ module.exports = {
   outputDir: "../src/main/resources/static",
   indexPath: "../static/index.html",
   devServer: {
-      port: 8081,
-      proxy: "http://localhost:8081"
+      port: 8082,
+      proxy: {
+        '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
+      }
   },
   chainWebpack: config => {
       const svgRule = config.module.rule("svg");
       svgRule.uses.clear();
       svgRule.use("vue-svg-loader").loader("vue-svg-loader");
   }
+}
 };
