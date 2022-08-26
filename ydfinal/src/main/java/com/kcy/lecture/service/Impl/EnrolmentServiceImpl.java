6e85bab97f9bce436f5e7be4612de8ca4df5dbf0
kimyonghyeon <mus5607@naver.com>
2022-08-26 17:56:51 +0900

    수강신청 22-08-26

--- ydfinal/src/main/java/com/kcy/lecture/service/Impl/EnrolmentServiceImpl.java
+++ ydfinal/src/main/java/com/kcy/lecture/service/Impl/EnrolmentServiceImpl.java
@@ -1,24 +1,42 @@
 package com.kcy.lecture.service.Impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kcy.lecture.mapper.EnrolmentMapper;
+import com.kcy.lecture.mapper.LectureMapper;
 import com.kcy.lecture.service.EnrolmentService;
 import com.kcy.lecture.service.EnrolmentVO;
+import com.kcy.lecture.service.OpenLectureVO;
 
 @Service
 public class EnrolmentServiceImpl implements EnrolmentService {
 
 	
 	@Autowired EnrolmentMapper mapper;
-	
+	@Autowired LectureMapper LectureMapper;
+
 	@Override
-	public List<EnrolmentVO> EnrolmentList(EnrolmentVO vo) {
-		
-		return mapper.EnrolmentList(vo) ;
+	public List<Map<String, String>> EnrolmentList(EnrolmentVO vo) {
+		return mapper.EnrolmentList(vo);
 	}
+
+	@Override
+	public void EnrolmentInsert(EnrolmentVO vo) {
+	
+		mapper.EnrolmentInsert(vo);
+	}
+
+
+
+	
+
+	
+
+
+
 
 }
