e69238bdc9078b3645fea022485e3b5c00501078
hwang <burnrare@naver.com>
2022-08-26 17:59:55 +0900

    공지 및 강의자료 게시판 등록 조회

--- ydfinal/src/main/java/com/kcy/notice/service/NoticeService.java
+++ ydfinal/src/main/java/com/kcy/notice/service/NoticeService.java
@@ -3,8 +3,6 @@ package com.kcy.notice.service;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
-
 public interface NoticeService {
 	public void NoticeInsert(NoticeVo vo);
 	public List<Map<String, String>> NoticeList(String id);
