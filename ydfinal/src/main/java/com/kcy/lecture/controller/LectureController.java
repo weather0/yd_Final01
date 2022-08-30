c582d06abdfdc49817e4f556d4ae218942ee2eb0
hwang <burnrare@naver.com>
2022-08-30 10:42:11 +0900

    ???

--- ydfinal/src/main/java/com/kcy/lecture/controller/LectureController.java
+++ ydfinal/src/main/java/com/kcy/lecture/controller/LectureController.java
@@ -10,6 +10,8 @@ import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +22,16 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kcy.file.test.FileDto;
@@ -41,7 +45,7 @@ import com.kcy.lecture.service.OpenLectureVO;
 
 import lombok.RequiredArgsConstructor;
 
-//김용현
+
 @Controller
 @RequiredArgsConstructor
 public class LectureController {
@@ -50,14 +54,19 @@ public class LectureController {
 	CourseMapper mapper;
 	private final LectureService LectureService;
 	private final EnrolmentService EnrolmentService;
-	// 하경
 	@Value("${spring.servlet.multipart.location}")
 	String filePath;
-
-<<<<<<< HEAD
+	
+	
 	Logger logger = LoggerFactory.getLogger(LectureController.class);
+	
+	@GetMapping("/lectureinsert")
+	public String LetureInsertPage(LectureVO vo) {
+		
+		return "pages/classMgr/LectureInsert";
+	}
+	
 
-=======
 	@PostMapping("/lectureinsert")
 	public String LetureInsert(LectureVO vo, @RequestParam("classFileSyl") MultipartFile classFileSyl, Model model) throws IllegalStateException, IOException {
 		
@@ -69,10 +78,8 @@ public class LectureController {
 						classFileSyl.getOriginalFilename(),
 						classFileSyl.getContentType());
 				String fileName = dto.getUuid() + "_" + dto.getFileName();
-				String oriFileName = classFileSyl.getOriginalFilename();
 				File newFileName = new File(fileName);
 				classFileSyl.transferTo(newFileName);
-				vo.setClassSylOriginal(oriFileName);
 				vo.setClassSyl(fileName);
 			}				
 			
@@ -82,104 +89,71 @@ public class LectureController {
 	}
 	
 	
->>>>>>> branch 'master' of https://github.com/weather0/yd_Final01
 	@ModelAttribute("course")
-	public List<CourseVO> getDepartments() {
-
+	public List<CourseVO> getDepartments(){
+		
+		
 		return mapper.getCourse();
 	}
 	
 	
-	// 강좌신청 페이지
-	@GetMapping("/lectureinsert")
-	public String LetureInsertPage(LectureVO vo) {
-
-		return "pages/classMgr/LectureInsert";
-	}
-	
-	//실제 강좌신청 추가하는 부분
-	@PostMapping("/lectureinsert")
-	public String LetureInsert(LectureVO vo, @RequestParam("classFileSyl") MultipartFile classFileSyl, Model model)
-			throws IllegalStateException, IOException {
-
-		logger.info(vo.toString());
-		//첨부파일 업로드 부분
-		if (!classFileSyl.isEmpty()) {
-			FileDto dto = new FileDto(UUID.randomUUID().toString(), classFileSyl.getOriginalFilename(),
-					classFileSyl.getContentType());
-			String fileName = dto.getUuid() + "_" + dto.getFileName();
-			File newFileName = new File(fileName);
-			classFileSyl.transferTo(newFileName);
-			vo.setClassSyl(fileName);
-		}
-
-		LectureService.LectureInsert(vo);
-
-		return "redirect:lecturelist";
-	}
-
-
-	//강좌신청 승인 페이지
 	@GetMapping("/lecturelist")
 	public String letureList(Model model) {
-		model.addAttribute("ltrList", LectureService.LectureList(null));
+		model.addAttribute("ltrList",LectureService.LectureList(null));
 		return "pages/classMgr/LectureList";
 	}
-
+	
+	
 	@PostMapping("/lectureupdate")
-	@ResponseBody
-	public boolean letureUpdate(LectureVO vo) {
-		//승인여부 업데이트 후  강좌신청완료 테이블에 추가
-		
+	public String letureUpdate(LectureVO vo) {
 		LectureService.LectureUpdate(vo);
-		
-		return true;
-		
+		LectureService.LectureInsertClass(vo);
+		return "redirect:lecturelist";
 	}
-
+	
 	@GetMapping("/download")
 	public ResponseEntity<Resource> download(@ModelAttribute LectureVO dto) throws IOException {
-
-
+		
+		System.out.println("ㅎㅇ");
+		
 		Path path = Paths.get(filePath + "/" + dto.getClassSyl());
 		String contentType = Files.probeContentType(path);
 		HttpHeaders headers = new HttpHeaders();
-
-		headers.setContentDisposition(
-				ContentDisposition.builder("attachment").filename(dto.getClassSyl(), StandardCharsets.UTF_8).build());
-
+		
+		headers.setContentDisposition(ContentDisposition.builder("attachment").
+				filename(dto.getClassSyl(), StandardCharsets.UTF_8)
+				.build());
+	
 		headers.add(HttpHeaders.CONTENT_TYPE, contentType);
 		Resource resource = new InputStreamResource(Files.newInputStream(path));
 		return new ResponseEntity<>(resource, headers, HttpStatus.OK);
 	}
 
-	//용현- 수강신청목록 리스트 
+	
 	@GetMapping("/openlecturelist")
-	public String OpenletureList(Model model, Principal principal, EnrolmentVO vo, OpenLectureVO vo1) {
+	public String OpenletureList(Model model, Principal principal,EnrolmentVO vo,OpenLectureVO vo1 ) {
 		vo.setUserId(principal.getName());
 		vo1.setUserId(principal.getName());
-
-		model.addAttribute("openList", LectureService.OpenLectureList(vo1));
+		
+		model.addAttribute("openList",LectureService.OpenLectureList(vo1));
 		model.addAttribute("enrolmentlist", EnrolmentService.EnrolmentList(vo));
-		model.addAttribute("credit", EnrolmentService.creitCheck(vo));
+		model.addAttribute("credit",EnrolmentService.creitCheck(vo));
 		return "pages/classMgr/OpenLectureList";
 	}
-
 	
 	@PostMapping("/openlectureinsert")
-	@ResponseBody
-	public boolean OpenlectureInsert(EnrolmentVO vo) {
-		//수강신청완료 테이블에 추가 후에 신청인원 업데이트
+	public String OpenlectureInsert(EnrolmentVO vo) {
+		EnrolmentService.TotalPlusUpdate(vo);
 		EnrolmentService.EnrolmentInsert(vo);
-		return true;
+		return "redirect:openlecturelist";
 	}
-
+	
 	@RequestMapping("/enrolmentdelete")
-	@ResponseBody
-	public boolean openlectureDelete(EnrolmentVO vo) {
-		//수강신청 취소 후에 신청인원 업데이트
+	public String openlectureDelete(EnrolmentVO vo) {
+		EnrolmentService.TotalMinusUpdate(vo);
 		EnrolmentService.EnrolmentDelete(vo);
-		return true;
+		return "redirect:openlecturelist";
 	}
+	
 
 }
